f1667bfa664924cfc4ea8abd394a13e55e8ec911
iBrahimHammadi <ibrahimsh2091@gmail.com>
2023-10-13 18:51:11 +0100

    Added the player with his movments

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
+def playerMovement():
+    global player_speed
+    player.x += player_speed
+
 #Creating the screen
 width, height = 800, 600
 Screen = pygame.display.set_mode((width, height))
@@ -10,17 +14,37 @@ pygame.display.set_caption('A Platformer Level')
 
 #Creating the ground
 ground = pygame.Rect(0, 540, width, height)
+#Creating the player
+player = pygame.Rect(width/2 - 15, 480, 30, 60 )
+#Creating the player Speed
+player_speed = 0
 
 #Colors
 cyan = (0,88,88)
 while True:
     
     for event in pygame.event.get():
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                player_speed += 5
+            if event.key == pygame.K_LEFT:
+                player_speed -= 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                player_speed -= 5
+            if event.key == pygame.K_LEFT:
+                player_speed += 5
+        
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+
     Screen.fill(cyan)
+    playerMovement()
     #Drawing the ground
-    pygame.draw.rect(Screen,(0, 0, 0), ground)
+    pygame.draw.rect(Screen,(200, 200, 200), ground)
+    pygame.draw.rect(Screen,(10, 10, 10), player)
     pygame.display.update()
     clock.tick(60)
